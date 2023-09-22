d0f192ba5af32df65b80356f711039da602670bb
Yujin Lee <yujinl9654@gmail.com>
2023-09-22 16:27:36 +0900

    [feat] loadingScreeen 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Login from './routes/login';
 import CreateAccount from './routes/create-account';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import { useEffect, useState } from 'react';
+import LoadingScreen from './components/loading-screen';
 
 const router = createBrowserRouter([
   {
@@ -41,10 +43,19 @@ body {
 `;
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const init = async () => {
+    // wait for firebase
+    setLoading(false);
+  };
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <>
       <GlobalStyles />
-      <RouterProvider router={router} />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
     </>
   );
 }
