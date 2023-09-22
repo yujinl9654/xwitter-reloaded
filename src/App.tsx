4fab36067ece4d959fec599c73300e55c2b963c7
Yujin Lee <yujinl9654@gmail.com>
2023-09-22 17:09:05 +0900

    [style] create account 페이지 작성

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from './routes/home';
 import Profile from './routes/profile';
 import Login from './routes/login';
 import CreateAccount from './routes/create-account';
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import { useEffect, useState } from 'react';
 import LoadingScreen from './components/loading-screen';
@@ -35,12 +35,19 @@ const router = createBrowserRouter([
 
 const GlobalStyles = createGlobalStyle`
 ${reset};
-* {box-sizing: border-box;}
+* {
+  box-sizing: border-box;
+}
 body {
   background-color: black;
   color: white;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
+`;
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
 `;
 
 function App() {
@@ -55,10 +62,10 @@ function App() {
   }, []);
 
   return (
-    <>
+    <Wrapper>
       <GlobalStyles />
       {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
-    </>
+    </Wrapper>
   );
 }
 
