74902772404576d33db4703c52052066fdd585a3
Yujin Lee <yujinl9654@gmail.com>
2023-09-22 16:35:14 +0900

    [chore] firebase 설치

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBKZdKaql2DrddzlE5oKIxVHlXezWA2m_g',
+  authDomain: 'xwitter-reloaded.firebaseapp.com',
+  projectId: 'xwitter-reloaded',
+  storageBucket: 'xwitter-reloaded.appspot.com',
+  messagingSenderId: '751506541570',
+  appId: '1:751506541570:web:092288e56f863bb9032b1b',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
