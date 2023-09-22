848fd26f2d71663873b5e1d4f1335c2a1718a3ef
yujin.lee <yujinl9654@gmail.com>
2023-09-22 21:21:56 +0900

    [feat] firebase sign in 기능 추가

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,8 @@
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { auth } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
 const Wrapper = styled.div`
   height: 100%;
@@ -38,6 +41,7 @@ const Error = styled.span`
 `;
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
   const [isLoading, setLoading] = useState(false);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -56,13 +60,27 @@ export default function CreateAccount() {
       setPassword(value);
     }
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    if (isLoading || name === '' || email === '' || password === '') return;
+
     try {
+      setLoading(true);
+
       // create an account
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(credentials.user);
+
       // set the name of the user
+      await updateProfile(credentials.user, { displayName: name });
+
       // redirect to the home page
+      navigate('/');
     } catch (e) {
       // setError
     } finally {
@@ -74,7 +92,7 @@ export default function CreateAccount() {
 
   return (
     <Wrapper>
-      <Title>Log into Xwitter</Title>
+      <Title>Join Xwitter</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
