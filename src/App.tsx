ea0904cc2060d4918276f3e42fd7d5626d368d13
Yujin Lee <yujinl9654@gmail.com>
2023-09-22 16:42:03 +0900

    [feat] firebase auth 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import { useEffect, useState } from 'react';
 import LoadingScreen from './components/loading-screen';
+import { auth } from './firebase';
 
 const router = createBrowserRouter([
   {
@@ -46,6 +47,7 @@ function App() {
   const [isLoading, setLoading] = useState(true);
   const init = async () => {
     // wait for firebase
+    await auth.authStateReady();
     setLoading(false);
   };
   useEffect(() => {
