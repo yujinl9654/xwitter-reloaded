00e35cf873c15fd681992fb0ee06713ece01441f
yujin.lee <yujinl9654@gmail.com>
2023-09-24 00:27:23 +0900

    [feat] 2MB 이하 이미지만 업로드 되도록 조건 추가

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -70,7 +70,9 @@ export default function PostTweetForm() {
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
     if (files && files.length === 1) {
-      setFile(files[0]);
+      files[0].size < 1024 * 1024 * 2
+        ? setFile(files[0])
+        : alert('Cannot upload over 2MB');
     }
   };
   const onSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
