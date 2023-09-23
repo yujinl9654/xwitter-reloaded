cc950fadd892d7bb8ff4d0cb5fb0d37ca91f70b4
yujin.lee <yujinl9654@gmail.com>
2023-09-24 00:11:34 +0900

    [feat] firestore에 저장 테스트

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,7 @@
+import { addDoc, collection } from 'firebase/firestore';
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { auth, db } from '../firebase';
 
 const Form = styled.form`
   display: flex;
@@ -70,9 +72,30 @@ export default function PostTweetForm() {
       setFile(files[0]);
     }
   };
+  const onSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const user = auth.currentUser;
+
+    if (!user || isLoading || tweet === '' || tweet.length > 180) return;
+
+    try {
+      setLoading(true);
+      await addDoc(collection(db, 'tweets'), {
+        tweet,
+        createdAt: Date.now(),
+        username: user.displayName || 'Anonymous',
+        userId: user.uid,
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <TextArea
         rows={5}
         maxLength={180}
